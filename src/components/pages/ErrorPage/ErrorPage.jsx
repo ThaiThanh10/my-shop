8c78391e2f24cabae1c30d5b1ee70d51e188726e
thaithanh10 <thaibathanh0610@gmail.com>
2023-10-22 09:51:29 +0700

    update header import errpage

--- src/components/pages/ErrorPage/ErrorPage.jsx
+++ src/components/pages/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, useNavigate } from "react-router-dom"
-import Header from "../../Elements/Header"
+import Header from "../../elements/Header"
 import Footer from "../../elements/Footer"
 
 const ErrorPage = () => {
